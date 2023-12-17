402f6a9791dc836448f3bfe15d3c41c5cdcee2c8
Hussam Albasha <hussam.dev.001@gmail.com>
2023-12-17 17:27:36 +0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import legacy from "@vitejs/plugin-legacy";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), legacy()],
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
+});
