05ddfc6caa4d4d18eea0e1cd5d6fc2277c5fe3e6
Hussam Albasha <hussam.dev.001@gmail.com>
2023-12-31 23:23:22 +0300

    favicon

--- dist/assets/ios.transition-legacy-062a5bc9.js
+++ dist/assets/ios.transition-legacy-062a5bc9.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-776adcfb.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.t,n=t.u}],execute:function(){
+System.register(["./index-legacy-2ba9e785.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.t,n=t.u}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- dist/assets/md.transition-legacy-7babf33e.js
+++ dist/assets/md.transition-legacy-7babf33e.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-776adcfb.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.u,i=e.t}],execute:function(){
+System.register(["./index-legacy-2ba9e785.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.u,i=e.t}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
