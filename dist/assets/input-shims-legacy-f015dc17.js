7ae6781083161b2ee48882378f494542a2d3efa4
Hussam Albasha <hussam.dev.001@gmail.com>
2024-01-02 23:42:13 +0300

    new Explore style

--- dist/assets/input-shims-legacy-f015dc17.js
+++ dist/assets/input-shims-legacy-f015dc17.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-2ba9e785.js"],(function(e,t){"use strict";var o,n,i,r,s,a,d,l,c,u,m;return{setters:[e=>{o=e.d,n=e.K,i=e.e,r=e.g,s=e.b,a=e.f,d=e.h,l=e.j,c=e.k,u=e.l,m=e.m}],execute:function(){
+System.register(["./index-legacy-5657cbb7.js"],(function(e,t){"use strict";var o,n,i,r,s,a,d,l,c,u,m;return{setters:[e=>{o=e.d,n=e.K,i=e.e,r=e.g,s=e.b,a=e.f,d=e.h,l=e.j,c=e.k,u=e.l,m=e.m}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
